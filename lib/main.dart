7aa58b62db44538403378903bf0aba91f3c4badb
aylinbas <e.aylin.bas@gmail.com>
2025-04-15 14:31:26 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
-import 'module/view/screens/sample_screen/sample_screen.dart';
-import 'module/view_model/sample_view_model.dart';
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -19,12 +16,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider.value(value: SampleViewModel()),
+        // ChangeNotifierProvider.value(value: SampleViewModel()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Sample Project',
-        home: SampleScreen(),
+        home: Container(),
       ),
     );
   }
